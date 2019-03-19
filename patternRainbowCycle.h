e90ab230f024d2c7bbca99c39e2138494569a5cb
jbellue <julien@bellue.fr>
2019-03-19 12:46:37 +0100

    get a working version of rainbow cycle it works on the divided strip, butit really need to not define the segments like that...

--- patternRainbowCycle.h
+++ patternRainbowCycle.h
@@ -7,14 +7,8 @@
 class patternRainbowCycle : public Pattern
 {
 private:
-    struct Segment {
-        uint16_t first;
-        uint16_t last;
-    };
-    static const uint8_t _num_segments = 3;
-    Segment _segments[_num_segments];
-    void initSegments();
-    /* data */
+    static const uint8_t _segmentCount = 3;
+    static const uint16_t _ledsPerSegment = 4;
 public:
     patternRainbowCycle(Adafruit_NeoPixel * strip);
     void update(const uint16_t index);
@@ -22,35 +16,19 @@ public:
     uint16_t interval();
 };
 
-void patternRainbowCycle::initSegments() {
-    const uint16_t pixelCount = _strip->numPixels();
-    uint8_t ledsPerSegment[_num_segments];
-    for (uint8_t i = 0; i < _num_segments; ++i) {
-        ledsPerSegment[i] = pixelCount / _num_segments;
-    }
-    const uint8_t remainder = _strip->numPixels() % _num_segments;
-    for (uint8_t i = 0; i < remainder; ++i) {
-        ledsPerSegment[i] += 1;
-    }
-    _segments[0].first = 0;
-    _segments[0].last = ledsPerSegment[0];
-    _segments[1].first = ledsPerSegment[0] + 1;
-    _segments[1].last = _segments[1].first + ledsPerSegment[1];
-    _segments[2].first = ledsPerSegment[1] + 1;
-    _segments[2].last = _segments[2].first + ledsPerSegment[2];
-}
 patternRainbowCycle::patternRainbowCycle(Adafruit_NeoPixel * strip) :
     Pattern(strip, 20, 255, 0x000000, 0x000000, "Rainbow Cycle") {
-    initSegments();
-}
+    }
 
 void patternRainbowCycle::start() {
     update(0);
 }
 void patternRainbowCycle::update(const uint16_t index) {
-    const uint16_t pixelCount = _strip->numPixels();
-    for(uint16_t i = 0; i < pixelCount; ++i) {
-        _strip->setPixelColor(i, colourWheel((i * 256 / pixelCount + index) & 255));
+    const uint16_t _segments[_segmentCount][_ledsPerSegment] = {{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}};
+    for(uint8_t i = 0; i < _segmentCount; ++i) {
+        for(uint16_t j = 0; j < _ledsPerSegment; ++j) {
+            _strip->setPixelColor(_segments[i][j], colourWheel((j * 256 / _ledsPerSegment + index) & 255));
+        }
     }
     _strip->show();
 }
